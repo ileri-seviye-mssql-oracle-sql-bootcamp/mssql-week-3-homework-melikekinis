6a9333b29210b3a6e90900f40cdab507b42d89d6
melikekinis <melikekinis@users.noreply.github.com>
2020-12-04 11:25:37 +0300

    Update Q3.sql

--- Q3.sql
+++ Q3.sql
@@ -6,11 +6,33 @@ FROM
 	[dbo].[tblMeslek] M
 INNER JOIN [dbo].[tblKullanici] K 
 	ON M.[meslekKod] = K.[meslekKod]
-WHERE K.[kullaniciKod] NOT IN 
+WHERE K.[kullaniciKod] NOT IN
 	(SELECT 
-		S.*, K.[kullaniciKod]
+		K.[kullaniciKod]
 	FROM 
 		[dbo].[tblKullanici] K
 	INNER JOIN dbo.tblSiparis S 
 		ON K.[kullaniciKod] = S.[kullaniciKod]
 	);
+	
+--- ya da ----
+
+SELECT 
+	M.[meslek]
+FROM 
+	[dbo].[tblMeslek] M
+INNER JOIN [dbo].[tblKullanici] K 
+	ON M.[meslekKod] = K.[meslekKod]
+WHERE K.[kullaniciKod] = (
+						SELECT 
+							K.[kullaniciKod]  
+						FROM 
+							[dbo].[tblKullanici] K
+						WHERE NOT EXISTS (
+										SELECT 1 
+										FROM 
+											[dbo].[tblSiparis] S
+										WHERE S.[kullaniciKod] = K.[kullaniciKod] --11365
+										 )
+						 );
+
